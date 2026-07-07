5777d87009def08ef8f35e33b1b8494d1bef1e1f
Anusha Deotale <anushadeotale@gamil.com>
2026-07-07 15:11:14 +0000

    Added Question 7

---
+++ program7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 25, num2 = 40;
+
+    if (num1 > num2)
+    {
+        printf("%d is the maximum number.\n", num1);
+    }
+    else
+    {
+        printf("%d is the maximum number.\n", num2);
+    }
+
+    return 0;
+}
