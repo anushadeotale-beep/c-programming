68ac6bd8b4a3813d261c61c23db85b517de478e1
Anusha Deotale <anushadeotale@gamil.com>
2026-07-07 07:59:35 +0000

    Added Question 2

---
+++ question2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int x = 17;
+    if (x%5==0)
+    {
+        printf("%dnumber is  divisible\n ",x);
+    }
+    else {
+        printf("%dthe number is not divisible\n ",x);
+
+    }
+    printf("\n");
+
+int y =50;
+if(y%5==0)
+{
+    printf("%dnumber is  divisible \n ",y);
+}
+else {
+    printf("%dthe number is not divisible \n ",y);
+
+}
+return 0;
+}
